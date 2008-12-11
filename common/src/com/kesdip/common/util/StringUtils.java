569002f0d1038777465d217ffe32a6a7b3e6db28
omnispot <>
2008-12-11 22:35:39 +0000

    common - Removed HBMs
    etc
    - Added web libraries
    - Added test DB data

---
+++ common/src/com/kesdip/common/util/StringUtils.java
@@ -0,0 +1,19 @@
+/*
+ * Disclaimer:
+ * Copyright 2008 - KESDIP E.P.E & Stelios Gerogiannakis - All rights reserved.
+ * eof Disclaimer
+ * 
+ * Date: Dec 1, 2008
+ * @author <a href="mailto:sgerogia@gmail.com">Stelios Gerogiannakis</a>
+ */
+package com.kesdip.common.util;
+
+/**
+ * Class offering string utility methods on top of those offered
+ * by Commons Utilities. 
+ * @author gerogias
+ */
+public class StringUtils extends org.apache.commons.lang.StringUtils {
+
+	
+}
