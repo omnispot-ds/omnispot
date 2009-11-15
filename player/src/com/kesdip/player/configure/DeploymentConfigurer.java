ed151d6b46f2c67146b430927d5bf0b97d84c176
omnispot <>
2009-11-15 10:14:25 +0000

    Implemented non-blocking i/o on System.in to avoid FlashComponent freeze

--- player/src/com/kesdip/player/configure/DeploymentConfigurer.java
+++ player/src/com/kesdip/player/configure/DeploymentConfigurer.java
@@ -9,7 +9,6 @@
 
 package com.kesdip.player.configure;
 
-import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 import org.apache.log4j.Logger;
@@ -33,6 +32,9 @@ import com.kesdip.player.Player;
  * @author gerogias
  */
 public class DeploymentConfigurer implements Runnable {
+	
+	private final static int BUFFER_SIZE = 512;
+	private final static int THREAD_CHECK_INTERVAL = 100;
 
 	/**
 	 * The logger.
@@ -54,7 +56,7 @@ public class DeploymentConfigurer implements Runnable {
 	public DeploymentConfigurer(Player player) {
 		this.player = player;
 	}
-
+	
 	/**
 	 * Endless loop reading from the standard in.
 	 * 
@@ -62,44 +64,92 @@ public class DeploymentConfigurer implements Runnable {
 	 */
 	@Override
 	public void run() {
-		BufferedReader reader = null;
+		InputStreamReader isr = null;
+		
+		char[] cb = new char[BUFFER_SIZE];
+		StringBuilder s = new StringBuilder(BUFFER_SIZE);
+		String line = null;
+		
 		try {
-			reader = new BufferedReader(new InputStreamReader(System.in,
-					"UTF-8"));
+			isr = new InputStreamReader(System.in, "UTF-8");
 			while (true) {
-				String line = reader.readLine();
-				// not a configuration line
-				if (StringUtils.isEmpty(line)) {
-					continue;
+				
+				// wait until there is some input
+				while (!isr.ready()) {
+					Thread.sleep(THREAD_CHECK_INTERVAL);
 				}
-				if (line.indexOf('=') == -1) {
-					logger.warn("Ignoring line: " + line);
-					continue;
+				
+				int n = isr.read(cb, 0, BUFFER_SIZE);
+				int nlIdx = -1;
+				int nextToBeConsumed = 0;
+				
+				// check if our newly received input has new lines in it and process them. 
+				for (int i = 0 ; i < n ; i++) {
+					if (cb[i] == '\n') {
+						
+						// remove CR if it exists
+						if (i > 0 && cb[i-1] == '\r') {
+							nlIdx = i - 1;
+						} else {
+							nlIdx = i;
+						}
+						
+						// assemble and process the line
+						s.append(cb, nextToBeConsumed, nlIdx - nextToBeConsumed);
+						line = s.toString();
+						
+						//logger.trace("consuming line |" + line + "|");
+						processLine(line);
+						
+						// empty the buffer
+						s.setLength(0);
+						
+						// remember where we are in processing the buffer
+						nextToBeConsumed = i + 1;
+					} 
 				}
-				ApplicationContext context = player.getDeploymentContext();
-				if (context == null) {
-					logger.warn("Deployment context not yet initialized");
-					continue;
-				}
-				String[] parts = line.split("\\=");
-				ApplicationContextBeanSetter setter = new ApplicationContextBeanSetter(
-						context);
-				try {
-					if (!setter.setValue(parts[0], parts[1])) {
-						logger.warn("Error setting expression: " + parts[0]);
-					}
-				} catch (FieldSetException fse) {
-					logger.warn("Error setting expression: " + parts[0]);
-				}
+				
+				// append the remainder of the buffer to the string builder
+				if (nextToBeConsumed < n)
+					s.append(cb, nextToBeConsumed, n - nextToBeConsumed);
+				
 			}
+			
 		} catch (Exception e) {
 			logger.error(e.getMessage());
 		} finally {
 			try {
-				reader.close();
+				isr.close();
 			} catch (Exception e) {
 				// do nothing
 			}
 		}
 	}
+	
+	private void processLine(String line) {
+		// not a configuration line
+		if (StringUtils.isEmpty(line)) {
+			return;
+		}
+		if (line.indexOf('=') == -1) {
+			logger.warn("Ignoring line: " + line);
+			return;
+		}
+		ApplicationContext context = player.getDeploymentContext();
+		if (context == null) {
+			logger.warn("Deployment context not yet initialized");
+			return;
+		}
+		String[] parts = line.split("\\=");
+		ApplicationContextBeanSetter setter = new ApplicationContextBeanSetter(
+				context);
+		try {
+			if (!setter.setValue(parts[0], parts[1])) {
+				logger.warn("Error setting expression: " + parts[0]);
+			}
+		} catch (FieldSetException fse) {
+			logger.warn("Error setting expression: " + parts[0]);
+		}
+	}
+	
 }
