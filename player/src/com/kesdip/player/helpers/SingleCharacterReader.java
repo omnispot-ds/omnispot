4d13972fadd84f3b2752d5cbed048af4d5a6d05f
omnispot <>
2010-02-21 22:35:55 +0000

    issue 138

--- player/src/com/kesdip/player/helpers/SingleCharacterReader.java
+++ player/src/com/kesdip/player/helpers/SingleCharacterReader.java
@@ -1,16 +1,12 @@
 package com.kesdip.player.helpers;
 
-import java.util.List;
-
 import org.apache.log4j.Logger;
 
 public class SingleCharacterReader {
 
 	Logger logger = Logger.getLogger(SingleCharacterReader.class);
 
-	List<String> charsList;
 	int charsIndex = 0;
-	int chars = 0;
 	boolean newcontent = false;
 	String newContent;
 	
@@ -26,17 +22,7 @@ public class SingleCharacterReader {
 	}
 
 	private void sliceToChars(String content) {
-		
 		characters = content.toCharArray();
-		
-		if (logger.isDebugEnabled()) {
-			logger.debug("number of chars: " + chars);
-			StringBuilder sb = new StringBuilder();
-			for (String ch :charsList)
-				sb.append(ch);
-
-			logger.debug(sb.toString());
-		}
 	}
 
 	/**
