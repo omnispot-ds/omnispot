a5645c917caf6c91638932452e87097085cb1f0d
omnispot <>
2010-03-10 19:06:21 +0000

    Fix default MPlayer path

--- designer/src/com/kesdip/designer/Activator.java
+++ designer/src/com/kesdip/designer/Activator.java
@@ -1,8 +1,12 @@
 package com.kesdip.designer;
 
+import java.io.File;
+
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
+
+import com.kesdip.designer.preferences.PreferenceConstants;
 
 /**
  * The activator class controls the plug-in life cycle
@@ -14,7 +18,7 @@ public class Activator extends AbstractUIPlugin {
 
 	// The shared instance
 	private static Activator plugin;
-	
+
 	/**
 	 * The constructor
 	 */
@@ -23,16 +27,24 @@ public class Activator extends AbstractUIPlugin {
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext
+	 * )
 	 */
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
+		// make sure the MPlayer path corresponds to a valid value
+		checkMPlayerPath();
 	}
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext
+	 * )
 	 */
 	public void stop(BundleContext context) throws Exception {
 		plugin = null;
@@ -41,7 +53,7 @@ public class Activator extends AbstractUIPlugin {
 
 	/**
 	 * Returns the shared instance
-	 *
+	 * 
 	 * @return the shared instance
 	 */
 	public static Activator getDefault() {
@@ -49,13 +61,32 @@ public class Activator extends AbstractUIPlugin {
 	}
 
 	/**
-	 * Returns an image descriptor for the image file at the given
-	 * plug-in relative path
-	 *
-	 * @param path the path
+	 * Returns an image descriptor for the image file at the given plug-in
+	 * relative path
+	 * 
+	 * @param path
+	 *            the path
 	 * @return the image descriptor
 	 */
 	public static ImageDescriptor getImageDescriptor(String path) {
 		return imageDescriptorFromPlugin(PLUGIN_ID, path);
+	}
+
+	/**
+	 * Check the path of MPlayer and make sure it exists. If not, set it
+	 * properly, relatively to the current directory. The current directory is
+	 * "c:/OmniSpot/AdminServer".
+	 */
+	private final void checkMPlayerPath() {
+		String mPlayerPath = this.getPreferenceStore().getString(
+				PreferenceConstants.P_MPLAYER_FILE);
+		File mPlayerFile = new File(mPlayerPath);
+		if (!mPlayerFile.isFile()) {
+			mPlayerFile = new File(System.getProperty("user.dir"), "mplayer/mplayer.exe");
+			this.getPreferenceStore().setValue(
+					PreferenceConstants.P_MPLAYER_FILE,
+					mPlayerFile.getAbsolutePath());
+		}
+
 	}
 }
